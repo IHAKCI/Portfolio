18277e1aba9e9c3131a6c77346cc7886671cc33f
IHAKCI <hamza23tr@gmail.com>
2025-08-23 13:50:28 +0000

    modification accueil "home"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,17 +12,19 @@ function Home() {
       <main className="main">
         <div className="presentation">
           <h1>Portfolio</h1>
-          <h2>Jessim</h2>
+          <h2>Hamza</h2>
           <p>
-            Bonjour, je m'appelle Jessim Laib et je suis en BTS SIO option SLAM,
-            une formation en deux ans axée sur le développement d’applications.
-            J’y apprends à analyser les besoins des utilisateurs, développer des
-            logiciels, gérer des bases de données et utiliser des langages comme
-            HTML, CSS, JavaScript, PHP ou Python. Ce BTS forme des techniciens
-            capables de créer des solutions numériques pour les entreprises,
-            avec des bases en cybersécurité et en gestion de projet. À la fin,
-            je pourrai travailler comme développeur ou poursuivre mes études en
-            informatique.
+           Bonjour, je m'appelle Hamza Kilinc et je suis en BTS SIO option SISR,
+          une formation en deux ans orientée vers l’administration des systèmes et des réseaux.
+          J’y apprends à installer, configurer et sécuriser des infrastructures informatiques,
+          gérer des serveurs, administrer des réseaux et utiliser des commandes sur terminaux
+          pour paramétrer des équipements comme les switchs ou les routeurs.
+          J’ai également eu l’occasion de toucher au développement web, ce qui complète mes compétences.
+          Ce BTS forme des techniciens capables de maintenir et d’optimiser des environnements informatiques
+          tout en tenant compte des enjeux de cybersécurité et de gestion de projet.
+          À l’avenir, je souhaite poursuivre mes études, au moins jusqu’au bachelor ou au master,
+          afin de me spécialiser dans l’administration systèmes et réseaux pour viser une augmentations 
+          de mes comptétences pour avoir une expertise dans le domaine .
           </p>
         </div>
 
