8ad866fe75039fc0e5c0754cb8545416b5fbd3c9
IHAKCI <hamza23tr@gmail.com>
2026-04-30 13:52:35 +0200

    commit modif Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,19 +6,6 @@ import Footer from "./Footer";
 import Preuve from "./Preuve";
 
 function Home() {
-  const [visible, setVisible] = useState(false);
-
-useEffect(() => {
-  const handleScroll = () => {
-    setVisible(window.scrollY > 300);
-  };
-  window.addEventListener("scroll", handleScroll);
-  return () => window.removeEventListener("scroll", handleScroll);
-}, []);
-
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
   return (
     <div>
       <Header />
@@ -28,16 +15,10 @@ const scrollToTop = () => {
           <h2>Hamza Kilinc</h2>
           <p>
             Bonjour en préparation de mon BTS SIO à l'institution des Chartreux, je vous présente mon portfolio qui regroupe mes projets scolaires et personnels.<br />
-  Vous y trouverez également mon CV et mes coordonnées via LinkedIn pour me contacter.<br />
-  Bonne visite !
+            Vous y trouverez également mon CV et mes coordonnées via LinkedIn pour me contacter.<br />
+            Bonne visite !
           </p>
-          {visible && (
-  <button onClick={scrollToTop} className="scroll-top-btn">
-    ↑
-  </button>
-)}
         </div>
-
         <p className="responsive">Version téléphone en travaux ;</p>
       </main>
       <Preuve />
